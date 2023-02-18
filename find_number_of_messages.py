b499755bd228f6379e63d6619d2cd32a383b7510
NurbekSaitqulov03 <nurbek.saitqulov23@gmail.com>
2023-02-18 16:37:58 +0500

    find_number_of_messages.py

--- find_number_of_messages.py
+++ find_number_of_messages.py
@@ -10,4 +10,10 @@ def find_number_of_messages(data: dict)->int:
         int: Total number of messages.
     
     """
-    return
+    x = 0
+    for i in data['messages']:
+        if i['type'] == 'message':
+            x+=1
+    return x
+data = read_data('data/result.json')
+print(find_number_of_messages(data))
